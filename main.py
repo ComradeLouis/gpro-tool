f213cc02c22b383ac145c6a41c40e174db0407fb
ComradeLouis <slfnicholson@gmail.com>
2024-06-30 14:30:46 +0100

    adding static data look up file

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from getData import *
+from staticData import *
 
 trackInfo = get_next_track()
 driverInfo = get_driver_info()
 carData,weather = get_qualy_and_race_data()
-
-print(trackInfo)
-print(driverInfo)
-print(carData)
-print(weather)
+trackData,partData = static_data_lookup(trackInfo['trackName'])
