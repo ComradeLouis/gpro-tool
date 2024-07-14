df5628a0ec8fa6f74c9bccbf536bc90b89ea796b
ComradeLouis <slfnicholson@gmail.com>
2024-07-14 17:16:02 +0100

    pushing file creation to separate function + file to clean up main

--- main.py
+++ main.py
@@ -19,18 +19,4 @@ totalLoss = calculate_time_loss(trackInfo,weather,fuelRequired)
 stratLoss,bestStrat = calculate_best_strategy(fuelRequired,tyreLife,trackInfo,weather,fuelPerLap)
 
 #write output json files
-setup_file = f"{trackInfo['trackName']}_R{officeData['race']}_setup.json"
-setup_path = f'output/S{officeData['season']}_setups'
-write_json(setup_path,setup_file,setupAndFuel)
-wear_file = f"{trackInfo['trackName']}_R{officeData['race']}_wear.json"
-wear_path = f'output/S{officeData['season']}_wear'
-write_json(wear_path,wear_file,carPartWear)
-loss_file = f"{trackInfo['trackName']}_R{officeData['race']}_loss.json"
-loss_path = f'output/S{officeData['season']}_loss'
-write_json(loss_path,loss_file,totalLoss)
-beststrat_file = f"{trackInfo['trackName']}_R{officeData['race']}_idealstrat.json"
-beststrat_path = f'output/S{officeData['season']}_strat'
-write_json(beststrat_path,beststrat_file,bestStrat)
-strat_file = f"{trackInfo['trackName']}_R{officeData['race']}_allstrat.json"
-strat_path = f'output/S{officeData['season']}_strat/S{officeData['season']}_allstrat'
-write_json(strat_path,strat_file,stratLoss)
+create_outputs(trackInfo,officeData,setupAndFuel,carPartWear,totalLoss,bestStrat,stratLoss)
